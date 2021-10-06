9fd12c1f95b62d8bce6846fe2bd05e912797e85c
mchester <marshall.chester@alumni.ubc.ca>
2021-10-05 21:51:05 -0700

    Completed up to Task 3.

--- mda_mchester.R
+++ mda_mchester.R
@@ -1,12 +1,13 @@
 
-#install packages and load libraries
-
+#install packages
 install.packages("devtools")
 devtools::install_github("UBC-MDS/datateachr")
+
+#load libraries
 library(datateachr)
 library(tidyverse)
 
-#explore selected datasets
+#task 1: choose favourite dataset
 #flow sample
 dim(flow_sample)
 class(flow_sample)
@@ -20,46 +21,37 @@ view(cancer_sample)
 #vancouver trees
 dim(vancouver_trees)
 class(vancouver_trees)
-print(vancouver_trees)
+view(vancouver_trees)
 
 #apartment buildings
 dim(apt_buildings)
 class(apt_buildings)
-print(apt_buildings)
+view(apt_buildings)
+
+#task 2: explore favourite dataset
+
+#explore relationship between 2 variables
+ggplot(cancer_sample, 
+       aes(area_mean, radius_mean)) +
+  geom_point(aes(color = diagnosis),
+             size = 3, 
+             alpha = 0.4)
+
+#create boxplot between 2 variables
+ggplot(cancer_sample, 
+       aes(x = diagnosis, y = symmetry_mean)) +
+  geom_boxplot(alpha = 0) +
+  geom_jitter(alpha = 0.3, colour = "tomato")
+
+#make new tibble of interesting variables
+tibble(cancer_sample) %>%
+  select(diagnosis, radius_mean, area_mean, smoothness_mean, symmetry_mean)
+
+#check distribution of area_mean
+ggplot(cancer_sample,
+       aes(area_mean)) +
+  geom_histogram(bins = 20)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-## R Markdown
-
-This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
-
-When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
-  
-  ```{r cars}
-summary(cars)
-```
-
-## Including Plots
-
-You can also embed plots, for example:
-  
-  ```{r pressure, echo=FALSE}
-plot(pressure)
-```
-
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
