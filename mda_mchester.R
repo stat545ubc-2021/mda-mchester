60b697c3672e1a222c0f79c99154000374e7d8a7
mchester <marshall.chester@alumni.ubc.ca>
2021-10-06 13:08:49 -0700

    Additions:
    -labels
    -legends
    -research questions

--- mda_mchester.R
+++ mda_mchester.R
@@ -1,3 +1,4 @@
+### This code analyses and explores data sets for STAT 545A Milestone 1
 
 #install packages
 install.packages("devtools")
@@ -31,26 +32,52 @@ view(apt_buildings)
 #task 2: explore favourite dataset
 
 #explore relationship between 2 variables
-ggplot(cancer_sample, 
-       aes(area_mean, radius_mean)) +
+  ggplot(cancer_sample, 
+       aes(x = area_mean, 
+           y = radius_mean, 
+           group = diagnosis)) +
   geom_point(aes(color = diagnosis),
              size = 3, 
-             alpha = 0.4)
+             alpha = 0.4) +
+    labs(x = "Area (Mean)", #add labels
+         y = "Radius (Mean)", 
+         title = "Radius (Mean) vs. Area (Mean)",
+         caption = "Source: UCI, 1995") +
+    theme(plot.title = element_text(hjust = 0.5)) + 
+    scale_color_discrete(name = "Diagnosis", #change legend
+                         labels=c("Benign", "Malignant"))
 
 #create boxplot between 2 variables
 ggplot(cancer_sample, 
-       aes(x = diagnosis, y = symmetry_mean)) +
+       aes(x = diagnosis,
+           y = symmetry_mean)) +
   geom_boxplot(alpha = 0) +
-  geom_jitter(alpha = 0.3, colour = "tomato")
+  geom_jitter(alpha = 0.3, colour = "tomato") + 
+  labs(x = "Diagnosis", #add labels
+       y = "Symmetry (Mean)", 
+       title = "Symmetry (Mean) vs Diagnosis",
+       caption = "Source: UCI, 1995") +
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  scale_x_discrete(breaks = c("B","M"), #change legend
+                   labels = c("Benign", "Malignant"))
 
 #make new tibble of interesting variables
 tibble(cancer_sample) %>%
   select(diagnosis, radius_mean, area_mean, smoothness_mean, symmetry_mean)
 
-#check distribution of area_mean
+#check density plot of area_mean
 ggplot(cancer_sample,
-       aes(area_mean)) +
-  geom_histogram(bins = 20)
+       aes(x = area_mean)) +
+  geom_density(aes(fill = diagnosis),
+               alpha = 0.8) +
+  labs(x = "Area (Mean)", #add labels
+       y = "Density", 
+       title = "Area (Mean) Density Plot",
+       caption = "Source: UCI, 1995") + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_fill_discrete(name = "Diagnosis",
+                      labels = c("Benign", "Malignant"))
+  
 
 
 
