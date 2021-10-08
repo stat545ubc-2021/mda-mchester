e2c5319da422c2e9c6a6743feba03a9a5f1ea5ac
mchester <marshall.chester@alumni.ubc.ca>
2021-10-08 16:14:16 -0700

    added: -facet -labels

--- mda_mchester.R
+++ mda_mchester.R
@@ -32,50 +32,53 @@ view(apt_buildings)
 
 #task 2: explore favourite dataset
 
-#explore relationship between 2 variables
-  ggplot(cancer_sample, 
-       aes(x = area_mean, 
+###2 variables
+ggplot(cancer_sample, #explore relationship between variables
+       aes(x = area_mean,
            y = radius_mean, 
            group = diagnosis)) +
   geom_point(aes(color = diagnosis),
-             size = 3, 
+             size = 3, #add transparency and change size
              alpha = 0.4) +
-    labs(x = "Area (Mean)", #add labels
-         y = "Radius (Mean)", 
-         caption = "Source: UCI, 1995") +
-    theme(plot.title = element_text(hjust = 0.5)) + 
-    scale_color_discrete(name = "Diagnosis", #change legend
-                         labels=c("Benign", "Malignant"))
+  labs(x = "Area (Mean)", #add labels
+       y = "Radius (Mean)", 
+       caption = "Source: UCI, 1995") +
+  scale_color_discrete(name = "Diagnosis", #change legend
+                       labels=c("Benign", "Malignant")) +
+  facet_wrap(~diagnosis) + #facet by row
+  theme(strip.text.x = element_blank())
+                                
 
-#create boxplot between 2 variables
-ggplot(cancer_sample, 
+###boxplot
+ggplot(cancer_sample, #create boxplot between 2 variables
        aes(x = diagnosis,
            y = symmetry_mean)) +
-  geom_boxplot(alpha = 0, 
+  geom_boxplot(alpha = 0, #add transparency and change width
                width = 0.25) +
-  geom_jitter(alpha = 0.3,
+  geom_jitter(alpha = 0.3, #add transparency and change width
               width =0.25) + 
   labs(x = "Diagnosis", #add labels
        y = "Symmetry (Mean)", 
        caption = "Source: UCI, 1995") +
-  scale_x_discrete(breaks = c("B","M"), #change legend
+  scale_x_discrete(breaks = c("B","M"), #change x tick
                    labels = c("Benign", "Malignant"))
 
-#make new tibble of interesting variables
-tibble(cancer_sample) %>%
-  select(diagnosis, radius_mean, area_mean, smoothness_mean, symmetry_mean)
+###tibble
+tibble(cancer_sample) %>% #make new tibble of interesting variables
+  select(diagnosis, radius_mean, perimeter_mean, area_mean, smoothness_mean, symmetry_mean)
 
-#check density plot of area_mean
-ggplot(cancer_sample,
-       aes(x = area_mean,
-           y = diagnosis)) +
-  ggridges::geom_density_ridges() +
+###density plot
+ggplot(cancer_sample, #check density plot of area_mean
+       aes(area_mean, fill = diagnosis)) +
+  geom_density(alpha = 0.6) +
   labs(x = "Area (Mean)", #add labels
-       y = "Diagnosis", 
+       y = "Density",
+       fill = "Diagnosis",
        caption = "Source: UCI, 1995") +
-  scale_y_discrete(breaks=c("B", "M"),
-                   labels=c("Benign", "Malignant"))
-  
+  scale_fill_discrete( #change legend titles
+    limits = c("B", "M"),
+    labels = c("Benign", "Malignant"))
+    
 
 
 
